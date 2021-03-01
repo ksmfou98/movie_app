71a5bc5bd8bef6a8bdd11cca992b44ba29a62b26
이도현 <64634992+ksmfou98@users.noreply.github.com>
2021-03-01 18:38:00 +0900

    movie_app plan

--- src/App.js
+++ src/App.js
@@ -1,29 +1,32 @@
 import React from 'react';
+import axios from 'axios';
 
 
-function Food({fav}){
-  console.log(fav);
-  return (
-    <h1>i like {fav} </h1>
-  )
+class App extends React.Component{
+  state = {
+    isLoading: true,
+    movies: []
+  }
+  
+  getMovies = async () => { //async, await 는 우리가 자바스크립트한테 axios 가 조금 시간이 걸릴수도 있다고 말해주는거임
+    const movies = await axios.get("https://yts-proxy.now.sh/list_movies.json");
+  };
 
-}
+  async componentDidMount(){   //render() 함수가 실행되고 나서 젤 먼저 실행되는 함수임.
+    this.getMovies();
+  }
+
+  render(){
+
+    const {isLoading} = this.state;
+    return (
+      <div>
+        {isLoading ? "Loading..." : "we are ready"}
+      </div>
+    )
+  }
 
 
-
-
-function App() {
-  return (
-    <div className="App">
-      <h1 id="222">dohyun</h1>
-      <span className="ddd">hyun</span>
-      hello! react
-      <Food fav="kimchi" />
-      <Food fav="ramen" />
-      <Food fav="gimbab" />
-      <Food fav="chicken" />
-    </div>
-  );
 }
 
 export default App;
